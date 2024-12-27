ad8c933690f36764ad614d058e1e25234da7284e
sanke <sanke@DESKTOP-0E8IKFC>
2024-12-27 15:31:51 +0530

    My First Commit

---
+++ Calculator/src/com/cjc/Calci.java
@@ -0,0 +1,12 @@
+package com.cjc;
+
+public class Calci {
+	int a=10;
+	int b=5;
+	int c;
+public void add()
+{
+	c=a+b;
+	System.out.println("addition of two numbers is: " +c);
+}
+}
---
+++ Calculator/src/com/cjc/Test.java
@@ -0,0 +1,10 @@
+package com.cjc;
+
+public class Test {
+	public static void main(String[] args)
+	{
+		Calci cal=new Calci();
+		cal.add();
+	}
+
+}
